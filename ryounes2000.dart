38104b5fbbe6055197de463c62d7503b6426f46c
rami younes <ryounes2000@gmail.com>
2021-02-10 02:09:16 +0200

    test2

---
+++ ryounes2000.dart
@@ -0,0 +1,16 @@
+void main(List<String> arguments) {
+
+
+  var myValue2 = 6;
+print (myNumber1(myValue2));
+print(MyFullName("rami azmi younes"));
+
+print(myEmplyeeName("rami","Younes",44));
+
+}
+
+int myNumber1 (x) => x*3;
+
+String MyFullName (name) => "my full name is: ${name}";
+
+String myEmplyeeName(name , lastname ,age) => "my name is : ${name} ${lastname} and old is : ${age}";
