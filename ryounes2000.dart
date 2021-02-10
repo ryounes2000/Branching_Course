620e16ae7e5c8ad1034d4f7dbc3963bb71dd94fc
rami younes <58847324+ryounes2000@users.noreply.github.com>
2021-02-10 03:20:14 +0200

    modified on ryounes2000 - without merge command

--- ryounes2000.dart
+++ ryounes2000.dart
@@ -8,9 +8,3 @@ print(MyFullName("rami azmi younes"));
 print(myEmplyeeName("rami","Younes",44));
 
 }
-
-int myNumber1 (x) => x*3;
-
-String MyFullName (name) => "my full name is: ${name}";
-
-String myEmplyeeName(name , lastname ,age) => "my name is : ${name} ${lastname} and old is : ${age}";
